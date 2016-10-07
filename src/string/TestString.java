ce9bf9ca4dd1cdc093815ca029006e2d7e1d837c
jiangxingbo <1051693729@qq.com>
2016-10-07 21:21:19 +0800

    提交String测试类

---
+++ src/string/TestString.java
@@ -0,0 +1,10 @@
+package string;
+
+/**
+ * Created by jiangxb on 2016/10/7.
+ */
+public class TestString {
+    public static void main(String[] args) {
+        System.out.println("TestString.main");
+    }
+}
